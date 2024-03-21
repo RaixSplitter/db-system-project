64a7eddf425bf116cfe20b4cc6d689ce45541be3
RaixSplitter <markus.bao.quan@gmail.com>
2024-03-21 10:17:23 +0100

    Divide responsibilities

---
+++ db/views.sql
@@ -0,0 +1,89 @@
+
+USE Dhe_Hejking_Store;
+
+-- VIEWS --
+-- 1. VIEW OrderView: shows order ID, staff name, customer ID, and order date.
+
+CREATE VIEW OrderView AS
+SELECT O.OrderID, OI.ORDERQUANTITY, SPI.FirstName AS StaffName, O.CustomerID, O.OrderDate
+FROM Orders O
+JOIN Staff S ON O.StaffID = S.StaffID
+JOIN ORDERITEM OI ON OI.ORDERID = O.ORDERID
+JOIN StaffPrivateInfo SPI ON S.StaffID = SPI.StaffID;
+
+
+-- 2. VIEW CustomerProductPreferences shows which products are preferred by 
+-- which customers and which staff member sells them.
+
+CREATE VIEW CustomerProductPreferences AS
+SELECT 
+    C.CustomerID,
+    C.FirstName AS CustomerFirstName,
+    C.Surname AS CustomerSurname,
+    P.ProductID,
+    P.ProductName,
+    SPI.FirstName AS StaffFirstName,
+    SPI.Surname AS StaffSurname
+FROM 
+    Customer C
+JOIN 
+    Orders O ON C.CustomerID = O.CustomerID
+JOIN 
+    OrderItem OI ON O.OrderID = OI.OrderID
+JOIN 
+    Products P ON OI.ProductID = P.ProductID
+JOIN 
+    Staff S ON O.StaffID = S.StaffID
+JOIN 
+    StaffPrivateInfo SPI ON S.StaffID = SPI.StaffID;
+    
+    
+-- 3. VIEW TopSellingStaff shows which staff member has sold
+-- the most products in each category.
+CREATE VIEW TopSellingStaff AS
+SELECT 
+    SPI.StaffID,
+    SPI.FirstName,
+    SPI.Surname,
+    C.CategoryID,
+    COUNT(*) AS TotalSales
+FROM 
+    StaffPrivateInfo SPI
+JOIN 
+    Orders O ON SPI.StaffID = O.StaffID
+JOIN 
+    OrderItem OI ON O.OrderID = OI.OrderID
+JOIN 
+    Products P ON OI.ProductID = P.ProductID
+JOIN 
+    Categories C ON P.ProductID = C.ProductID
+GROUP BY 
+    SPI.StaffID, SPI.FirstName, SPI.Surname, C.CategoryID
+ORDER BY 
+    TotalSales DESC;
+
+-- VIEW 4 MostSoldBrandAndBuyer: shows which brand is the most sold and who buys it
+CREATE VIEW MostSoldBrandAndBuyer AS
+SELECT 
+    P.Brand,
+    COUNT(*) AS TotalSales,
+    C.CustomerID,
+    CONCAT(C.FirstName, ' ', C.Surname) AS CustomerName,
+    SPI.FirstName AS StaffFirstName,
+    SPI.Surname AS StaffSurname
+FROM 
+    Orders O
+JOIN 
+    OrderItem OI ON O.OrderID = OI.OrderID
+JOIN 
+    Products P ON OI.ProductID = P.ProductID
+JOIN 
+    Customer C ON O.CustomerID = C.CustomerID
+LEFT JOIN 
+    Staff S ON O.StaffID = S.StaffID
+LEFT JOIN 
+    StaffPrivateInfo SPI ON S.StaffID = SPI.StaffID
+GROUP BY 
+    P.Brand, C.CustomerID, C.FirstName, C.Surname, SPI.FirstName, SPI.Surname
+ORDER BY 
+    TotalSales DESC;
